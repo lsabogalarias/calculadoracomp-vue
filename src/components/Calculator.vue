89129c5fb4b81f459c3f0fdaea95e5bbc120785d
Lina <lsabogalarias@gmail.com>
2023-08-24 18:41:22 -0500

    Creacion de metodos AC y DEL

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -8,28 +8,28 @@
       </div>
 
       <div class="grid grid-cols-4 grid-rows-5 shadow-2xl rounded-b-lg">
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600">AC</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">DEL</button>
-        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800">%</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600" @click="clickAC()">AC</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="clickDEL()">DEL</button>
+        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('%')">%</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">7</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">8</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">9</button>
-        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800">X</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(7)">7</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(8)">8</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(9)">9</button>
+        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('*')">*</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">4</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">5</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">6</button>
-        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800">-</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(4)">4</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(5)">5</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(6)">6</button>
+        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('-')">-</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">3</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">2</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">1</button>
-        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800">+</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(3)">3</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(2)">2</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(1)">1</button>
+        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('+')">+</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600">0</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">.</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600">=</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600" @click="click(0)">0</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('.')">.</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('=')">=</button>
       </div>
     </div>      
     
@@ -45,7 +45,21 @@
       }
     },
     methods:{
-
+      clickAC(){
+        this.getResult = 0; 
+      },
+      clickDEL(){
+        if(this.getResult.length > 1)
+          this.getResult = this.getResult.substring(0, this.getResult.length -1);  
+        else
+          this.getResult = 0;   
+      },
+      click(number){
+        if(this.getResult == 0)
+          this.getResult = number;
+        else
+          this.getResult += number;
+      }
     }
   };
   </script>  
