7c16cca619aff57b41be6d5e0ebddeed4ee8b74c
Lina <lsabogalarias@gmail.com>
2023-08-24 21:10:03 -0500

    Adicion de metodos

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -10,26 +10,26 @@
       <div class="grid grid-cols-4 grid-rows-5 shadow-2xl rounded-b-lg">
         <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600" @click="clickAC()">AC</button>
         <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="clickDEL()">DEL</button>
-        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('%')">%</button>
+        <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('/')">/</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(7)">7</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(8)">8</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(9)">9</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('7')">7</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('8')">8</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('9')">9</button>
         <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('*')">*</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(4)">4</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(5)">5</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(6)">6</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('4')">4</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('5')">5</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('6')">6</button>
         <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('-')">-</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(3)">3</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(2)">2</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click(1)">1</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('3')">3</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('2')">2</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('1')">1</button>
         <button class="h-16 text-xl text-white font-semibold bg-gray-700 border-gray-600 hover:bg-gray-800" @click="click('+')">+</button>
 
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600" @click="click(0)">0</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 col-span-2 hover:bg-green-600" @click="click('0')">0</button>
         <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('.')">.</button>
-        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="click('=')">=</button>
+        <button class="h-16 text-xl text-white font-semibold bg-green-500 border-green-400 hover:bg-green-600" @click="clickResult('=')">=</button>
       </div>
     </div>      
     
@@ -59,6 +59,9 @@
           this.getResult = number;
         else
           this.getResult += number;
+      },
+      clickResult(){
+        this.getResult = eval(this.getResult);
       }
     }
   };
